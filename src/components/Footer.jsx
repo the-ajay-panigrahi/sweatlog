0588dc12dfdd5fdfb48d3b7fc2e7a855db6a02e8
the-ajay-panigrahi <ajaybpanigrahi@gmail.com>
2025-06-07 18:15:32 +0530

    feat: add footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,11 +2,21 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer>
-      <p>
-        &copy; {new Date().getFullYear()} SweatLog. Helping you stay on track.
-      </p>
-      <p>Keep moving. Keep logging. Keep growing.</p>
+    <footer
+      className="border-t border-slate-300"
+      style={{
+        boxShadow:
+          "0 -4px 6px -1px rgba(0,0,0,0.1), 0 -2px 4px -1px rgba(0,0,0,0.06)",
+      }}
+    >
+      <div className="max-w-[1000px] mx-auto flex flex-col sm:flex-row gap-2 justify-center items-center text-center px-4 py-5">
+        <p className="text-slate-900 font-thin">
+          &copy; {new Date().getFullYear()} SweatLog. Helping you stay on track.
+        </p>
+        <p className="text-slate-900 font-thin">
+          Keep moving. Keep logging. Keep growing.
+        </p>
+      </div>
     </footer>
   );
 };
