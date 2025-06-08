cee16f7eb0cc493dd3467b603a0558d8a6f0a1f3
the-ajay-panigrahi <ajaybpanigrahi@gmail.com>
2025-06-08 22:11:42 +0530

    feat: add modal section

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -3,17 +3,40 @@ import ReactDOM from "react-dom";
 
 const Modal = ({ showExerciseDescription, handleCloseModal }) => {
   const { name, description } = showExerciseDescription || {};
+
   return ReactDOM.createPortal(
-    <div>
-      <button onClick={handleCloseModal} />
-      <div>
-        <div>
-          <h6>Name</h6>
-          <h2>{name.replaceAll("-", "ajay")}</h2>
+    <div
+      className="fixed z-50 inset-0 bg-black/40 flex justify-center items-center px-4 cursor-pointer"
+      onClick={handleCloseModal} // click outside to close
+    >
+      <div
+        className="bg-white rounded-xl shadow-lg p-6 w-full max-w-md animate-fadeIn scale-95 cursor-default"
+        onClick={(e) => e.stopPropagation()} // Prevent click inside modal from closing
+      >
+        <div className="flex justify-between items-center mb-4">
+          <h3 className="text-xl font-semibold text-slate-800">
+            Exercise Info
+          </h3>
+          <button
+            onClick={handleCloseModal}
+            className= "cursor-pointer text-slate-400 hover:text-red-500 transition duration-200 text-2xl"
+          >
+            &times;
+          </button>
         </div>
+
+        <div className="mb-4">
+          <h6 className="text-sm text-slate-500">Name</h6>
+          <h2 className="text-lg font-bold text-slate-800 break-words">
+            {name?.replaceAll("-", " ")}
+          </h2>
+        </div>
+
         <div>
-          <h6>Description</h6>
-          <p>{description}</p>
+          <h6 className="text-sm text-slate-500">Description</h6>
+          <p className="text-base text-slate-700 whitespace-pre-line">
+            {description}
+          </p>
         </div>
       </div>
     </div>,
