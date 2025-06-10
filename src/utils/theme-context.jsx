a867cb95d2d9f5129dc7a7308031c8bb54a18c7f
the-ajay-panigrahi <ajaybpanigrahi@gmail.com>
2025-06-10 22:21:39 +0530

    feat: minor ui/ux improvements

--- src/utils/theme-context.jsx
+++ src/utils/theme-context.jsx
@@ -3,9 +3,15 @@ import { createContext, useState } from "react";
 export const ThemeContext = createContext({});
 
 export const ThemeContextProvider = ({ children }) => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(
+    () => localStorage.getItem("theme") === "dark"
+  );
   const toggleTheme = () => {
-    setIsDarkMode(!isDarkMode);
+    setIsDarkMode((prevMode) => {
+      const newMode = !prevMode;
+      localStorage.setItem("theme", newMode ? "dark" : "light");
+      return newMode;
+    });
   };
   const value = { isDarkMode, toggleTheme };
   return (
